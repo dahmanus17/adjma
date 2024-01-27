735683d14134eed8110e2764bc0314cfca3f9b5e
adjm <you@gmail.com>
2024-01-27 12:47:02 -0500

    share post functionality

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,13 +1,19 @@
+// Import necessary modules and components
 import { useRouter } from "next/router";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
 import { formatDistanceToNowStrict } from "date-fns";
-
 import useLoginModal from "@/hooks/useLoginModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLike from "@/hooks/useLike";
-
 import Avatar from "../Avatar";
+
+import { RiShareForwardLine } from "react-icons/ri";
+import { RiFileCopyLine } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import { toast } from 'react-hot-toast';
+
+
 interface PostItemProps {
   data: Record<string, any>;
   userId?: string;
@@ -45,6 +51,17 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
     [loginModal, currentUser, toggleLike]
   );
 
+  const openPopup = (ev: React.MouseEvent<HTMLDivElement>) => {
+    ev.stopPropagation();
+    setPopupOpen(true);
+  
+  };
+
+  const closePopup = (ev: React.MouseEvent) => {
+    ev.stopPropagation();
+    setPopupOpen(false);
+  };
+
   const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
 
   const createdAt = useMemo(() => {
@@ -54,6 +71,28 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
 
     return formatDistanceToNowStrict(new Date(data.createdAt));
   }, [data.createdAt]);
+
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const copyToClipboard = (ev: React.MouseEvent<SVGElement, MouseEvent>) => {
+    ev.stopPropagation();
+    const textToCopy = `https://www.adjma.com/posts/${data.id}`;
+
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    document.body.appendChild(textArea);
+
+    textArea.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(textArea);
+    toast.success('Post link copied',{duration:3000});
+    closePopup(ev);
+  };
+
+  const preventPropagation = (ev: React.MouseEvent<HTMLDivElement>) => {
+    ev.stopPropagation();
+  };
 
   return (
     <div
@@ -146,9 +185,53 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
               <LikeIcon color={hasLiked ? "red" : ""} size={20} />
               <p>{data.likedIds.length}</p>
             </div>
+
+            <div
+              id="sharePostButton"
+              onClick={openPopup}
+              className="
+                flex 
+                flex-row 
+                items-center 
+                text-neutral-500 
+                gap-2 
+                cursor-pointer 
+                transition 
+                hover:text-[#29ab87]
+            "
+            >
+              <RiShareForwardLine size={22} />
+            </div>
           </div>
         </div>
       </div>
+
+      {/* Popup content */}
+      {isPopupOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50 cursor-default"
+          onClick={preventPropagation}
+        >
+          <div className="bg-gray-800 text-white p-5 rounded-md relative w-full max-w-xs sm:max-w-md">
+            <button
+              onClick={closePopup}
+              className="absolute top-2 right-2 text-gray-500"
+            >
+              <AiOutlineClose size={20} />
+            </button>
+            <div className="flex items-center mt-8 mb-5">
+              <p className="flex-grow overflow-hidden whitespace-nowrap text-ellipsis">
+                https://www.adjma.com/posts/{data.id}
+              </p>
+              <RiFileCopyLine
+                onClick={copyToClipboard}
+                className="cursor-pointer"
+                size={30}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
