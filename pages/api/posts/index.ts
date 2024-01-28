a7879b38a836caa5d0b8c07402a4253594311edd
adjm <you@gmail.com>
2024-01-28 17:53:10 -0500

    specific post on top

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -5,68 +5,79 @@ import prisma from "@/libs/prismadb";
 
 const API_KEY = process.env.API_KEY;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST' && req.method !== 'GET') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST" && req.method !== "GET") {
     return res.status(405).end();
   }
-  
 
   try {
-
-    const apiKey = req.headers['x-api-key'];
+    const apiKey = req.headers["x-api-key"];
 
     // Check if the API key is present and correct
-    if(req.method !== 'POST'){
+    if (req.method !== "POST") {
       if (!apiKey || apiKey !== API_KEY) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: "Unauthorized" });
       }
     }
-    
-    
-    if (req.method === 'POST') {
+
+    if (req.method === "POST") {
       const { currentUser } = await serverAuth(req, res);
       const { body } = req.body;
 
       const post = await prisma.post.create({
         data: {
           body,
-          userId: currentUser.id
-        }
+          userId: currentUser.id,
+        },
       });
 
       return res.status(200).json(post);
     }
 
-    if (req.method === 'GET') {
+    if (req.method === "GET") {
       const { userId } = req.query;
 
       let posts;
 
-      if (userId && typeof userId === 'string') {
+      if (userId && typeof userId === "string") {
         posts = await prisma.post.findMany({
           where: {
-            userId
+            userId,
           },
           include: {
             user: true,
-            comments: true
+            comments: true,
           },
           orderBy: {
-            createdAt: 'desc'
+            createdAt: "desc",
           },
         });
       } else {
         posts = await prisma.post.findMany({
           include: {
             user: true,
-            comments: true
+            comments: true,
           },
           orderBy: {
-            createdAt: 'desc'
-          }
+            createdAt: "desc",
+          },
         });
       }
+      //to put a specific post at the top
+      const index = posts.findIndex(
+        (obj) => obj.id === "65b6bbf60f3f92b54c353c4f"
+      );
 
+      if (index !== -1) {
+        const movedObject = posts.splice(index, 1)[0];
+        posts.unshift(movedObject);
+      } else {
+        console.log("Object not found");
+      }
+      //end
       return res.status(200).json(posts);
     }
   } catch (error) {
