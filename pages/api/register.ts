3603f5fece7e51fff24b6f6fbd530121bd367063
adjm <you@gmail.com>
2024-01-31 18:26:04 -0500

    limit using adjma for creating account

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -11,8 +11,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).end();
   }
 
+  const { email, username, name, password } = req.body;
+
+  if (name.toLowerCase().includes('adjma')) {
+    return res.status(400).end();
+  }
+
   try {
-    const { email, username, name, password } = req.body;
+    //const { email, username, name, password } = req.body;
 
     // Calculate Gravatar URL
     const gravatarHash = md5(email.trim().toLowerCase());
