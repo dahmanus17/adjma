2688c20840f0da81ebe0d07833e2d2d085e366bd
adjm <you@gmail.com>
2024-05-12 16:02:49 -0400

    enleve le pages and people dans followbar

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -10,9 +10,9 @@ const FollowBar = () => {
   }
 
   return (
-    <div className="px-6 py-4 hidden lg:block">
+    <div className="px-6 py-4 hidden lg:block h-screen overflow-y-scroll">
       <div className="bg-neutral-800 rounded-xl p-4">
-        <h2 className="text-white text-xl font-semibold">People & Pages</h2>
+{/* <h2 className="text-white text-xl font-semibold">People & Pages</h2> */}
         <div className="flex flex-col gap-6 mt-4">
           {users.map((user: Record<string, any>) => (
             <div key={user.id} className="flex flex-row gap-4">
