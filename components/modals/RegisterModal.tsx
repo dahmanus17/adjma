03785537a879c2ba3f17ef65a3f74388b62a2f29
adjm <you@gmail.com>
2024-05-12 12:16:15 -0400

    register input validate

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -6,6 +6,8 @@ import useRegisterModal from "@/hooks/useRegisterModal";
 import axios from "axios";
 import toast from "react-hot-toast";
 import {signIn} from "next-auth/react";
+import Button from "../Button";
+//import { error } from "console";
 
 const RegisterModal = () => {
   const loginModal = useLoginModal();
@@ -26,7 +28,97 @@ const RegisterModal = () => {
     loginModal.onOpen();
   }, [loginModal, registerModal, isLoading]);
 
+  const isPasswordValid = (password: string): boolean => {
+    const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
   const onSubmit = useCallback(async () => {
+    const emailError = document.querySelector('#email-error');
+    const nameError = document.querySelector('#name-error');
+    const usernameError = document.querySelector('#username-error');
+    const passwordError = document.querySelector('#password-error');
+    
+    if ((emailError as HTMLElement).style.display !== 'none') {
+      if(emailError){
+        emailError.textContent = '';
+      (emailError as HTMLElement).style.display = 'none';
+      }
+    }
+
+    if ((nameError as HTMLElement).style.display !== 'none') {
+      if(nameError){
+        nameError.textContent = '';
+      (nameError as HTMLElement).style.display = 'none';
+      }
+    }
+
+    if ((usernameError as HTMLElement).style.display !== 'none') {
+      if(usernameError){
+        usernameError.textContent = '';
+      (usernameError as HTMLElement).style.display = 'none';
+      }
+    }
+
+    if ((passwordError as HTMLElement).style.display !== 'none') {
+      if(passwordError){
+        passwordError.textContent = '';
+      (passwordError as HTMLElement).style.display = 'none';
+      }
+    }
+
+    //on regarde si champs vides
+    if(!email){
+      if(emailError){
+        (emailError as HTMLElement).style.display = '';
+        emailError.textContent = 'Please enter an email.';
+      }
+      return;
+    }
+    if(!name){
+      if(nameError){
+        (nameError as HTMLElement).style.display = '';
+        nameError.textContent = 'Please enter a name.';
+      }
+      return;
+    }
+    if(!username){
+      if(usernameError){
+        (usernameError as HTMLElement).style.display = '';
+        usernameError.textContent = 'Please enter a username.';
+      }
+      return;
+    }
+    if(!password){
+      if(passwordError){
+        (passwordError as HTMLElement).style.display = '';
+        passwordError.textContent = 'Please enter a password.';
+      }
+      return;
+    }
+    
+    if (!isPasswordValid(password)) {
+      if (passwordError) {
+        (passwordError as HTMLElement).style.display = '';
+        if(password.length < 8) {
+          passwordError.textContent = 'Password must contain at least 8 characters.';
+          return;
+        }
+        if (!/[a-zA-Z]/.test(password)) {
+          passwordError.textContent = 'Password must contain at least one letter.';
+          return;
+        }
+        if (!/[0-9]/.test(password)) {
+          passwordError.textContent = 'Password must contain at least one number.';
+          return;
+        }
+        if (!/[!@#$%^&*()_+]/.test(password)) {
+          passwordError.textContent = 'Password must contain at least one special character.';
+          return;
+        }
+      }
+      //return;
+    }
     try {
       setIsLoading(true);
       
@@ -47,8 +139,23 @@ const RegisterModal = () => {
       });
 
       registerModal.onClose();
-    } catch (error) {
-      toast.error('Something went wrong');
+    } catch (error:any) {
+      if(error.response.data.message === "Invalid Email") {
+        
+    if (emailError) {
+      (emailError as HTMLElement).style.display = '';
+      emailError.textContent = error.response.data.message;
+    }
+      }
+
+    if(error.response.data.message === "Invalid Username") {
+        
+    if (usernameError) {
+      (usernameError as HTMLElement).style.display = '';
+      usernameError.textContent = error.response.data.message;
+    }
+      }
+      //toast.error(error.response.data.message);
     } finally {
       setIsLoading(false);
     }
@@ -62,18 +169,21 @@ const RegisterModal = () => {
         value={email}
         disabled={isLoading}
       />
+      <span id="email-error" style={{ color: "red", display: "none", fontFamily: "Arial, sans-serif", fontSize: "14px" }}></span>
       <Input
         disabled={isLoading}
         placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <span id="name-error" style={{ color: "red", display: "none" }}></span>
       <Input
         disabled={isLoading}
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <span id="username-error" style={{ color: "red", display: "none" }}></span>
       <Input
         placeholder="Password"
         type="password"
@@ -81,7 +191,9 @@ const RegisterModal = () => {
         value={password}
         disabled={isLoading}
       />
+      <span id="password-error" style={{ color: "red", display: "none" }}></span>
     </div>
+    
   );
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -17,6 +17,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).end();
   }
 
+  const isUserExist = await prisma.user.findFirst({
+    where: {
+        OR: [
+            {
+                email,
+            },
+            {
+                username,
+            },
+        ],
+    },
+  });
+
+  if (isUserExist) {
+    if(isUserExist.email === email) {
+      return res.status(400).json({ message: "Invalid Email" });
+    }
+    if(isUserExist.username === username) {
+      return res.status(400).json({ message: "Invalid Username" });
+    }
+  }
+
   try {
     //const { email, username, name, password } = req.body;
 
@@ -38,7 +60,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     return res.status(200).json(user);
   } catch (error) {
-    //console.log(error);
     return res.status(400).end();
   }
 }
